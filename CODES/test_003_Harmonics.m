bbcb75be787aadc561ebddaf11fd7e67b5f1b17e
Archit Gupta <archit.guota@gmail.com>
2015-02-19 16:26:10 +0530

    Initialized Harmonic Analysis

--- CODES/test_003_Harmonics.m
+++ CODES/test_003_Harmonics.m
@@ -2,11 +2,27 @@ close all;
 clear all;
 clc;
 
-INSTRUMENTS = {'Viola'; 'Violin'; 'Guitar'; 'Ukulele'; 'Bass'; 'Banjo'};
-BASE_ADDRS  = '../CASES/';
+%%  CONSTANT DECLARATIONS
+
+AIFF_SCALE_FACTOR   = .001;
+
+%%  EXTRACTING MUSICAL DATA AND RESAMPLING
+
+base_addr   = '../CASES/';
+instruments = dir(base_addr);
+instruments = instruments(3:end); 
 
 i           = 1;
 j           = 1;
 
-ADDR        = strcat(BASE_ADDRS,INSTRUMENTS{i}, '/', int2str(j), '.wav');
-file        = wavread(ADDR);
+pieces 	    = dir(strcat(base_addr,instruments(i).name)); 
+pieces      = pieces(3:end);
+ 	
+addr        = strcat(base_addr,'/',instruments(i).name,'/',pieces(j).name);
+file        = double(aiffread(addr))*AIFF_SCALE_FACTOR;
+
+FS      = 441;
+FC      = 100;
+
+S       = resample(file, FC, FS);
+
